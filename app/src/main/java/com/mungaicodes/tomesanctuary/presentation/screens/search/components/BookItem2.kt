3ed57634b3195ad8d2680325e7c1defe281a44c1
mmungai <mungai_official@outlook.com>
2022-12-28 14:29:55 +0300

    info card added

--- app/src/main/java/com/mungaicodes/tomesanctuary/presentation/screens/search/components/BookItem2.kt
+++ app/src/main/java/com/mungaicodes/tomesanctuary/presentation/screens/search/components/BookItem2.kt
@@ -1,11 +1,15 @@
 package com.mungaicodes.tomesanctuary.presentation.screens.search.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -18,41 +22,85 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.mungaicodes.tomesanctuary.R
 import com.mungaicodes.tomesanctuary.domain.model.Book
+import com.mungaicodes.tomesanctuary.presentation.ui.theme.SelectedItem
 
 @Composable
 fun BookItem2(
     book: Book,
 ) {
-
-    Card(Modifier.size(140.dp, 220.dp)) {
-        Box(Modifier.fillMaxSize()) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(book.volumeInfo?.imageLinks?.thumbnail?.replace("http", "https"))
-                    .crossfade(true)
-                    .placeholder(R.drawable.loading_animation)
-                    .error(R.drawable.ic_broken_image)
-                    .build(),
-                contentScale = ContentScale.Crop,
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize()
-            )
-            Scrim(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
-                    .align(Alignment.BottomCenter)
-            )
-            book.volumeInfo?.let {
-                Text(
-                    text = it.title,
-                    color = MaterialTheme.colors.onPrimary,
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .align(Alignment.BottomStart),
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        Modifier
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
                 )
+            )
+    ) {
+        Column {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .size(140.dp, 220.dp)
+                    .animateContentSize(
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(book.volumeInfo?.imageLinks?.thumbnail?.replace("http", "https"))
+                        .crossfade(true)
+                        .placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image)
+                        .build(),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+                Scrim(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .align(Alignment.BottomCenter)
+                )
+                book.volumeInfo?.let {
+                    Row(modifier = Modifier.align(Alignment.BottomStart)) {
+                        Text(
+                            text = it.title,
+                            color = MaterialTheme.colors.onPrimary,
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .weight(0.75f),
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        ItemExpandButton(
+                            expanded = expanded,
+                            onClick = { expanded = !expanded }
+                        )
+                    }
+                }
+            }
+            if (expanded) {
+                Card(
+                    elevation = 4.dp,
+                    border = BorderStroke(2.dp, SelectedItem),
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(top = 4.dp),
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .padding(4.dp)
+                    ) {
+                        Text(text = book.volumeInfo?.maturityRating!!)
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/mungaicodes/tomesanctuary/presentation/screens/search/components/ItemExpandButton.kt
@@ -0,0 +1,25 @@
+package com.mungaicodes.tomesanctuary.presentation.screens.search.components
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ItemExpandButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = "click to expand for more details",
+        )
+    }
+}
