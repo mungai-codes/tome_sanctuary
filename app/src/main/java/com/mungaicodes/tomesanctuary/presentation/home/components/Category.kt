346f63aa2f099bfdff04999e082b443a8fa879cf
mmungai <mungai_official@outlook.com>
2023-01-16 14:40:21 +0300

    added category images

--- app/src/main/java/com/mungaicodes/tomesanctuary/presentation/home/components/Category.kt
+++ app/src/main/java/com/mungaicodes/tomesanctuary/presentation/home/components/Category.kt
@@ -4,64 +4,151 @@ import androidx.annotation.DrawableRes
 import com.mungaicodes.tomesanctuary.R
 
 data class Category(
-    val title: String,
-    val description: String = "...",
-    val query: String = "Android",
+    val label: String,
+    val value: String = "...",
     @DrawableRes
     val image: Int
 )
 
 val categories = listOf(
     Category(
-        title = "Code",
-        image = R.drawable.code_icon
+        label = "Architecture",
+        image = R.drawable.architecture,
+        value = "ARCHITECTURE"
     ),
     Category(
-        title = "Drama",
-        image = R.drawable.code_icon
+        label = "Art",
+        image = R.drawable.art,
+        value = "ART"
     ),
     Category(
-        title = "Cooking",
-        image = R.drawable.code_icon
+        label = "Auto/Bio-graphy",
+        image = R.drawable.bio,
+        value = "BIOGRAPHY & AUTOBIOGRAPHY"
     ),
     Category(
-        title = "Education",
-        image = R.drawable.code_icon
+        label = "Business & Economics",
+        image = R.drawable.economics,
+        value = "BUSINESS & ECONOMICS"
     ),
     Category(
-        title = "Art and Literature",
-        image = R.drawable.code_icon
+        label = "Comics & Graphic Novels",
+        image = R.drawable.comics,
+        value = "COMICS & GRAPHIC NOVELS"
     ),
     Category(
-        title = "Fantasy",
-        image = R.drawable.code_icon
+        label = "Computers",
+        image = R.drawable.computers,
+        value = "COMPUTERS"
     ),
     Category(
-        title = "Fiction",
-        image = R.drawable.code_icon
+        label = "Cooking",
+        image = R.drawable.food,
+        value = "COOKING"
     ),
     Category(
-        title = "Romance",
-        image = R.drawable.code_icon
+        label = "Design",
+        image = R.drawable.design,
+        value = "DESIGN"
     ),
     Category(
-        title = "History",
-        image = R.drawable.code_icon
+        label = "Drama",
+        image = R.drawable.drama,
+        value = "DRAMA"
     ),
     Category(
-        title = "Autobiography",
-        image = R.drawable.code_icon
+        label = "Education",
+        image = R.drawable.education,
+        value = "EDUCATION"
     ),
     Category(
-        title = "Graphic Novel",
-        image = R.drawable.code_icon
+        label = "Fiction",
+        image = R.drawable.fiction,
+        value = "FICTION"
     ),
     Category(
-        title = "Children's Literature",
-        image = R.drawable.code_icon
+        label = "History",
+        image = R.drawable.history,
+        value = "HISTORY"
     ),
     Category(
-        title = "Mystery",
-        image = R.drawable.code_icon
+        label = "Humor",
+        image = R.drawable.humor,
+        value = "HUMOR"
+    ),
+    Category(
+        label = "LAW",
+        image = R.drawable.law,
+        value = "LAW"
+    ),
+    Category(
+        label = "True Crime",
+        image = R.drawable.crime,
+        value = "TRUE CRIME"
+    ),
+    Category(
+        label = "Travel",
+        image = R.drawable.travel,
+        value = "TRAVEL"
+    ),
+    Category(
+        label = "Self-Help",
+        image = R.drawable.help,
+        value = "SELF-HELP"
+    ),
+    Category(
+        label = "Science",
+        image = R.drawable.science,
+        value = "SCIENCE"
+    ),
+    Category(
+        label = "Religion",
+        image = R.drawable.religion,
+        value = "RELIGION"
+    ),
+    Category(
+        label = "Psychology",
+        image = R.drawable.psychology,
+        value = "PSYCHOLOGY"
+    ),
+    Category(
+        label = "Poetry",
+        image = R.drawable.poetry,
+        value = "POETRY"
+    ),
+    Category(
+        label = "Philosophy",
+        image = R.drawable.philosophy,
+        value = "PHILOSOPHY"
+    ),
+    Category(
+        label = "Pets",
+        image = R.drawable.pets,
+        value = "PETS"
+    ),
+    Category(
+        label = "Nature",
+        image = R.drawable.nature,
+        value = "NATURE"
+    ),
+    Category(
+        label = "Music",
+        image = R.drawable.music,
+        value = "MUSIC"
+    ),
+    Category(
+        label = "Medical",
+        image = R.drawable.health,
+        value = "MEDICAL"
+    ),
+    Category(
+        label = "Maths ",
+        image = R.drawable.maths,
+        value = "MATHEMATICS"
+    ),
+    Category(
+        label = "Literary Criticism",
+        image = R.drawable.criticism,
+        value = "LITERARY CRITICISM"
     ),
 )
